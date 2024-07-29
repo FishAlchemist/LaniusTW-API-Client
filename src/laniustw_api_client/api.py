0557cde1cd34fb572272e93667e949ae0c0808aa
FishAlchemist <48265002+FishAlchemist@users.noreply.github.com>
2024-07-29 14:40:24 +0800

    Remove print. Cancel read time for every request, Except API test

--- src/laniustw_api_client/api.py
+++ src/laniustw_api_client/api.py
@@ -8,7 +8,6 @@ import pathlib
 @validate_call
 def test(identifier: uuid.UUID | None) -> httpx.Response:
     api_url = f"{laniustw_api_client.PROJECT_API_URL}/api/test"
-    print(api_url)
     params = {"identifier": identifier}
     return httpx.get(url=api_url, params=params)
 
@@ -17,14 +16,14 @@ def test(identifier: uuid.UUID | None) -> httpx.Response:
 def speech_recogn(speech_file: pathlib.Path) -> httpx.Response:
     api_url = f"{laniustw_api_client.PROJECT_API_URL}/api/speech_recognition"
     files = {"file": (speech_file.name, speech_file.open(mode="rb"))}
-    return httpx.post(url=api_url, files=files)
+    return httpx.post(url=api_url, files=files, timeout=httpx.Timeout(10.0, read=None))
 
 
 @validate_call
 def image_recognition(file: pathlib.Path) -> httpx.Response:
     api_url = f"{laniustw_api_client.PROJECT_API_URL}/api/Image_recognition"
     files = {"file": (file.name, file.open("rb"))}
-    return httpx.post(url=api_url, files=files)
+    return httpx.post(url=api_url, files=files, timeout=httpx.Timeout(10.0, read=None))
 
 
 @validate_call
@@ -34,7 +33,7 @@ def chat_RAG(question: str, identifier: uuid.UUID | None) -> httpx.Response:
         "question": question,
         "identifier": identifier,
     }
-    return httpx.get(url=api_url, params=params)
+    return httpx.get(url=api_url, params=params, timeout=httpx.Timeout(10.0, read=None))
 
 
 @validate_call
@@ -47,7 +46,7 @@ def chat_Recommendation(
         "identifier": identifier,
         "Recommended_num": Recommended_num,
     }
-    return httpx.get(url=api_url, params=params)
+    return httpx.get(url=api_url, params=params, timeout=httpx.Timeout(10.0, read=None))
 
 
 @validate_call
@@ -57,4 +56,4 @@ def chat_Experience(question: str, identifier: uuid.UUID | None):
         "question": question,
         "identifier": identifier,
     }
-    return httpx.get(url=api_url, params=params)
+    return httpx.get(url=api_url, params=params, timeout=httpx.Timeout(10.0, read=None))
