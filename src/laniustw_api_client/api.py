8a0bf855a0cd2e5036e2b98941125dac357e0bcc
FishAlchemist <48265002+FishAlchemist@users.noreply.github.com>
2024-08-02 21:24:55 +0800

    Add Token for api token check.

--- src/laniustw_api_client/api.py
+++ src/laniustw_api_client/api.py
@@ -6,58 +6,63 @@ import pathlib
 
 
 @validate_call
-def test(identifier: uuid.UUID | None) -> httpx.Response:
+def test(identifier: uuid.UUID | None, token: str) -> httpx.Response:
     api_url = f"{laniustw_api_client.PROJECT_API_URL}/api/test"
-    params = {"identifier": identifier}
+    params = {"identifier": identifier, "token": token}
     return httpx.get(url=api_url, params=params)
 
 
 @validate_call
-def speech_recogn(speech_file: pathlib.Path | str) -> httpx.Response:
+def speech_recogn(speech_file: pathlib.Path | str, token: str) -> httpx.Response:
     if isinstance(speech_file, str):
         speech_file = pathlib.Path(speech_file)
     api_url = f"{laniustw_api_client.PROJECT_API_URL}/api/speech_recognition"
     files = {"file": (speech_file.name, speech_file.open(mode="rb"))}
-    return httpx.post(url=api_url, files=files, timeout=httpx.Timeout(10.0, read=None))
+    return httpx.post(
+        url=api_url,
+        files=files,
+        timeout=httpx.Timeout(10.0, read=None),
+        params={"token": token},
+    )
 
 
 @validate_call
-def image_recognition(file: pathlib.Path | str) -> httpx.Response:
+def image_recognition(file: pathlib.Path | str, token: str) -> httpx.Response:
     if isinstance(file, str):
         file = pathlib.Path(file)
     api_url = f"{laniustw_api_client.PROJECT_API_URL}/api/Image_recognition"
     files = {"file": (file.name, file.open("rb"))}
-    return httpx.post(url=api_url, files=files, timeout=httpx.Timeout(10.0, read=None))
+    return httpx.post(
+        url=api_url,
+        files=files,
+        timeout=httpx.Timeout(10.0, read=None),
+        params={"token": token},
+    )
 
 
 @validate_call
-def chat_RAG(question: str, identifier: uuid.UUID | None) -> httpx.Response:
+def chat_RAG(question: str, identifier: uuid.UUID | None, token: str) -> httpx.Response:
     api_url = f"{laniustw_api_client.PROJECT_API_URL}/api/chat/RAG"
-    params = {
-        "question": question,
-        "identifier": identifier,
-    }
+    params = {"question": question, "identifier": identifier, "token": token}
     return httpx.get(url=api_url, params=params, timeout=httpx.Timeout(10.0, read=None))
 
 
 @validate_call
 def chat_Recommendation(
-    question: str, identifier: uuid.UUID | None, Recommended_num: int | None
+    question: str, identifier: uuid.UUID | None, Recommended_num: int | None, token: str
 ) -> httpx.Response:
     api_url = f"{laniustw_api_client.PROJECT_API_URL}/api/chat/Recommendation"
     params = {
         "question": question,
         "identifier": identifier,
         "Recommended_num": Recommended_num,
+        "token": token,
     }
     return httpx.get(url=api_url, params=params, timeout=httpx.Timeout(10.0, read=None))
 
 
 @validate_call
-def chat_Experience(question: str, identifier: uuid.UUID | None):
+def chat_Experience(question: str, identifier: uuid.UUID | None, token: str):
     api_url = f"{laniustw_api_client.PROJECT_API_URL}/api/chat/Experience"
-    params = {
-        "question": question,
-        "identifier": identifier,
-    }
+    params = {"question": question, "identifier": identifier, "token": token}
     return httpx.get(url=api_url, params=params, timeout=httpx.Timeout(10.0, read=None))
