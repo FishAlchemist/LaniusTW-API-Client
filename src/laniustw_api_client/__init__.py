9adfe3bc24b34951b4807fe8f9ebb48bee974aa8
FishAlchemist <48265002+FishAlchemist@users.noreply.github.com>
2024-09-11 22:29:08 +0800

    Match API Server version v2.0.

--- src/laniustw_api_client/__init__.py
+++ src/laniustw_api_client/__init__.py
@@ -2,6 +2,6 @@ import typing
 
 PROJECT_API_URL: str | None = None
 """e.g. https://www.example.com"""
-API_SERVER_VERSION: typing.Final[str] = "1.4"
+API_SERVER_VERSION: typing.Final[str] = "2.0"
 """Implemented API Server version"""
 del typing
--- src/laniustw_api_client/api.py
+++ src/laniustw_api_client/api.py
@@ -1,66 +1,70 @@
 import httpx
 import uuid
-from pydantic import validate_call
+import laniustw_model.response
+import pydantic
 import laniustw_api_client
+import typing
 import pathlib
 
+model_type = typing.TypeVar("model_type")
 
-@validate_call
-def health_check(token: str) -> httpx.Response:
+
+class ModelResponse(typing.Generic[model_type]):
+    @pydantic.validate_call
+    def __init__(self, response: httpx.Response, result_type: model_type) -> None:
+        super().__init__()
+        self.response: httpx.Response = response
+        self.result_type: model_type = result_type
+
+    @property
+    def result(self) -> model_type:
+        if issubclass(self.result_type, pydantic.BaseModel):
+            return self.result_type.model_validate(self.response.json())
+        return self.result_type(self.response.text)
+
+
+@pydantic.validate_call
+def health_check(token: str) -> ModelResponse[laniustw_model.response.HealthCheck]:
     api_url = f"{laniustw_api_client.PROJECT_API_URL}/api/health"
-    return httpx.get(url=api_url, params={"token": token})
-
-
-@validate_call
-def speech_recogn(speech_file: pathlib.Path | str, token: str) -> httpx.Response:
-    if isinstance(speech_file, str):
-        speech_file = pathlib.Path(speech_file)
-    api_url = f"{laniustw_api_client.PROJECT_API_URL}/api/speech_recognition"
-    files = {"speech_file": (speech_file.name, speech_file.read_bytes())}
-    return httpx.post(
-        url=api_url,
-        files=files,
-        timeout=httpx.Timeout(10.0, read=None),
-        params={"token": token},
+    response = httpx.get(url=api_url, params={"token": token})
+    return ModelResponse[laniustw_model.response.HealthCheck](
+        response=response, result_type=laniustw_model.response.HealthCheck
     )
 
 
-@validate_call
-def image_recognition(file: pathlib.Path | str, token: str) -> httpx.Response:
-    if isinstance(file, str):
-        file = pathlib.Path(file)
-    api_url = f"{laniustw_api_client.PROJECT_API_URL}/api/Image_recognition"
-    files = {"file": (file.name, file.read_bytes())}
-    return httpx.post(
-        url=api_url,
-        files=files,
-        timeout=httpx.Timeout(10.0, read=None),
-        params={"token": token},
+@pydantic.validate_call
+def integrate(Images: list[pathlib.Path], speech_file: pathlib.Path, token: str) -> str:
+    api_url = f"{laniustw_api_client.PROJECT_API_URL}/api/Integrate"
+    files = [("Images", (image.name, image.read_bytes())) for image in Images]
+    files.append(("speech_file", (speech_file.name, speech_file.read_bytes())))
+    params = {"token": token}
+    response = httpx.get(
+        url=api_url, params=params, timeout=httpx.Timeout(20.0, read=None)
     )
+    return ModelResponse[str](response=response, result_type=str)
 
 
-@validate_call
-def chat_RAG(question: str, identifier: uuid.UUID | None, token: str) -> httpx.Response:
+@pydantic.validate_call
+def chat_RAG(
+    question: str, identifier: uuid.UUID | None, token: str
+) -> ModelResponse[laniustw_model.response.ChatResponse[uuid.UUID]]:
     api_url = f"{laniustw_api_client.PROJECT_API_URL}/api/chat/RAG"
     params = {"question": question, "identifier": identifier, "token": token}
-    return httpx.get(url=api_url, params=params, timeout=httpx.Timeout(10.0, read=None))
+    response = httpx.get(
+        url=api_url, params=params, timeout=httpx.Timeout(10.0, read=None)
+    )
+    return ModelResponse[laniustw_model.response.ChatResponse[uuid.UUID]](
+        response=response, result_type=laniustw_model.response.ChatResponse[uuid.UUID]
+    )
 
 
-@validate_call
-def chat_Recommendation(
-    question: str, Recommended_num: int | None, token: str
-) -> httpx.Response:
-    api_url = f"{laniustw_api_client.PROJECT_API_URL}/api/chat/Recommendation"
-    params = {
-        "question": question,
-        "Recommended_num": Recommended_num,
-        "token": token,
-    }
-    return httpx.get(url=api_url, params=params, timeout=httpx.Timeout(10.0, read=None))
-
-
-@validate_call
+@pydantic.validate_call
 def chat_Experience(question: str, identifier: uuid.UUID | None, token: str):
     api_url = f"{laniustw_api_client.PROJECT_API_URL}/api/chat/Experience"
     params = {"question": question, "identifier": identifier, "token": token}
-    return httpx.get(url=api_url, params=params, timeout=httpx.Timeout(10.0, read=None))
+    response = httpx.get(
+        url=api_url, params=params, timeout=httpx.Timeout(10.0, read=None)
+    )
+    return ModelResponse[laniustw_model.response.ChatResponse[uuid.UUID]](
+        response=response, result_type=laniustw_model.response.ChatResponse[uuid.UUID]
+    )
