137579478be9791c70b47c84cb75c728109ec4c2
FishAlchemist <48265002+FishAlchemist@users.noreply.github.com>
2024-10-25 20:14:08 +0800

    Mark supported Server v4.

--- src/laniustw_api_client/__init__.py
+++ src/laniustw_api_client/__init__.py
@@ -1,7 +1,10 @@
 import typing
+import packaging.version
 
 PROJECT_API_URL: str | None = None
 """e.g. https://www.example.com"""
-API_SERVER_VERSION: typing.Final[str] = "3.0"
+API_SERVER_VERSION: typing.Final[packaging.version.Version] = packaging.version.Version(
+    "4.0.0"
+)
 """Implemented API Server version"""
 del typing
