80dad030ae120c14f852c716ab0e6864414f1441
FishAlchemist <48265002+FishAlchemist@users.noreply.github.com>
2024-11-02 11:14:04 +0800

    Rename image_recogn to image_recognition

--- src/laniustw_api_client/api.py
+++ src/laniustw_api_client/api.py
@@ -43,7 +43,7 @@ def speech_recogn(speech_file: pathlib.Path, token: str):
 
 
 @pydantic.validate_call
-def image_recogn(images: list[pathlib.Path], token: str):
+def image_recognition(images: list[pathlib.Path], token: str):
     API_URL = f"{laniustw_api_client.PROJECT_API_URL}/api/image-recogn"
     files = [("Images", (image.name, image.read_bytes())) for image in images]
     params = {"token": token}
